c30bedfe8e63b5e011071988b3da8cfdfb26b02f
dengemann90 <72465696+dengemann90@users.noreply.github.com>
2021-04-30 15:13:28 +0200

    Prerequisite database connection

---
+++ src/main/java/com/example/demo/utils/DbCred.java
@@ -0,0 +1,9 @@
+package com.example.demo.utils;
+
+public interface DbCred {
+    final String driverClass = "oracle.jdbc.driver.OracleDriver";
+    final String url = "xxx";
+    final String user = "xxx";
+    final String password = "xxx";
+    final String schema = "xxx";
+}
---
+++ src/main/java/com/example/demo/utils/JdbcUtils.java
@@ -0,0 +1,104 @@
+package com.example.demo.utils;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class JdbcUtils {
+
+    private static final Logger L = LoggerFactory.getLogger(JdbcUtils.class);
+
+    public static void loadDriver(final String driver) {
+        try {
+            Class.forName(driver);
+            L.info("driver <" + driver + "> loaded");
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static Connection getConnectionViaDriverManager(final String url, final String user, final String passwd) {
+        try {
+            Connection connection = DriverManager.getConnection(url, user, passwd);
+            L.info("connection got");
+            return connection;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static Connection getConnectionViaDataSource(final DataSource ds) {
+        try {
+            Connection connection = ds.getConnection();
+            L.info("connection got");
+            return connection;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void closeConnection(final Connection connection) {
+        try {
+            if (connection != null) {
+                connection.close();
+                L.info("connection closed");
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void closeConnectionQuietly(final Connection connection) {
+        try {
+            closeConnection(connection);
+        } catch (Exception e) {
+            // ignore exception, just log
+            L.error("unhandled", e);
+        }
+    }
+
+    public static void closeResultSet(final ResultSet resultSet) {
+        try {
+            if (resultSet != null) {
+                resultSet.close();
+                L.info("result set closed");
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void closeResultSetQuietly(final ResultSet resultSet) {
+        try {
+            closeResultSet(resultSet);
+        } catch (RuntimeException e) {
+            // ignore exception, just log
+            L.error("unhandled", e);
+        }
+    }
+
+    public static void closeStatement(final Statement statement) {
+        try {
+            if (statement != null) {
+                statement.close();
+                L.info("statement closed");
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void closeStatementQuietly(final Statement statement) {
+        try {
+            closeStatement(statement);
+        } catch (RuntimeException e) {
+            // ignore exception, just log
+            L.error("unhandled", e);
+        }
+    }
+}
