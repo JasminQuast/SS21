faaef68dcb4a40d3e714047d28ecbfe79a2e671c
JasminQuast <jasmin.quast@student.htw-berlin.de>
2021-04-30 12:31:46 +0200

    add class User and interface UserRepository + Umgebungsvariablen

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,10 +1,15 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
 public class HelloController {
+
+        @Autowired
+        private Environment enw;
 
         @RequestMapping("/")
         public String index() {
