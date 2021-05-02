c8de20e5b1b3541fd13ec4885cf5d78f158e9c10
dengemann90 <72465696+dengemann90@users.noreply.github.com>
2021-05-02 14:34:03 +0200

    Postgres DB Heroku connected - Oracle DB deleted

--- src/main/java/com/example/demo/utils/DbCred.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.utils;
-
-public interface DbCred {
-    final String driverClass = "oracle.jdbc.driver.OracleDriver";
-    final String url = "xxx";
-    final String user = "xxx";
-    final String password = "xxx";
-    final String schema = "xxx";
-}
